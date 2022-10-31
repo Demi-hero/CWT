724ced37422be721a8f476c521679cd137075ab4
Demi-hero <scott.pearce3@gmail.com>
2022-10-31 21:42:31 +0000

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -10,24 +10,28 @@ if __name__ == '__main__':
     args = parser.parse_args()
     version = args.version
     dir = os.path.join("models", f"version_{args.version}")
+    model_file = os.path.join(dir, "model.pickle")
+    model_acc = os.path.join(dir, "model_accuracy.csv")
     if not os.path.isdir(dir):
         # stand in for actual logging
         print("There is no model registered to this version")
-
-    model_file = os.path.join(dir, "model.pickle")
-    model_acc = os.path.join(dir, "model_accuracy.csv")
     if not os.path.isfile(model_file):
         print("There is no model in the version. Check calibration status")
     if not os.path.isfile(model_acc):
         print("There is not accuracy data. Test the model accuracy before deployment")
 
-    flask_model = os.path.join("Flask", 'model', "model.pickle")
+    new_flask_model = os.path.join("Flask", 'model', "model_new.pickle")
+    flask_model = os.path.join("Flask", 'model', "model_new.pickle")
+    new_flask_acc = os.path.join("Flask", 'model', "new_model_accuracy.csv")
     flask_acc = os.path.join("Flask", 'model', "model_accuracy.csv")
 
     # copy model accros
-    os.replace(model_file, flask_model)
-    shutil.copy(model_file, flask_model)
-    shutil.copy(model_acc, flask_acc)
-    # delete model os.remove()
-    # delet acc data
-    # rename new model
+    if len(os.listdir("Flask\\model")) == 0:
+        shutil.copyfile(model_file, flask_model)
+        shutil.copyfile(model_acc, flask_acc)
+    else:
+        shutil.copyfile(model_file, new_flask_model)
+        shutil.copyfile(model_acc, new_flask_acc)
+        os.replace(new_flask_model, flask_model)
+        os.replace(flask_acc, new_flask_acc)
+
